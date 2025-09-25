23b1acf61e1116985b2d54dbca64ad9813bffe69
Harry Franklin <hrfranklin0702@gmail.com>
2025-09-25 20:23:05 +0100

    New Assets
    Assets added and named, with prefabs made.
    
    PropRandomiser now randomly scales props between .95 and 1.5 x the prefab's scale.

--- NorseMythology-Project/Assets/SCRIPTS/World/Environment/PropRandomiser.cs
+++ NorseMythology-Project/Assets/SCRIPTS/World/Environment/PropRandomiser.cs
@@ -20,6 +20,15 @@ public class PropRandomiser : MonoBehaviour
             GameObject propPrefab = propPrefabs[randomIndex];
             GameObject prop = Instantiate(propPrefab, spawnPoint.transform.position, Quaternion.identity);
             prop.transform.SetParent(spawnPoint.transform);
+
+            float randomScale = (float)GetRandomNumber(0.95, 1.5);
+            prop.transform.localScale *= randomScale;
         }
+    }
+
+    public double GetRandomNumber(double minimum, double maximum)
+    { 
+        System.Random random = new System.Random();
+        return random.NextDouble() * (maximum - minimum) + minimum;
     }
 }
